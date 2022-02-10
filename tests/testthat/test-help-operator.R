4e3c99f6cad6d0315058cb3344beafbb5fa86a15
D-Se <2869830202@qq.com>
2022-02-10 16:46:32 +0800

    ternary ? unit tests

--- tests/testthat/test-help-operator.R
+++ tests/testthat/test-help-operator.R
@@ -2,6 +2,28 @@ test_that("?-operator returns correctly", {
   x <- c(T, T, F)
   y <- c(9, 5, 1)
   z <- c(1, 2, 3)
-  res <- c(9, 5, 3)
-  expect_equal(ifelse(x, y, z), c(T, T, F) ? c(9, 5, 1) ~ c(1, 2, 3))
+  # res <- c(9, 5, 3)
+  # ternary ? equals to vectorized ifelse
+  expect_equal(
+    ifelse(x, y, z),
+    c(T, T, F) ? c(9, 5, 1) ~ c(1, 2, 3)
+    )
+  
+  # ternary ? equals to single if .. else
+  expect_equal(
+    if(5 > 3) 10 else 3,
+    5 > 3 ? 10 ~ 3
+    )
+  
+  # binary ? converts types without dots passed
+  expect_equal(
+    as.character(y),
+    y ?~ chr
+    )
+  
+  # binary ? converts types with dots passed
+  expect_equal(
+    as.integer(y, 2, 3),
+    y ?~ int(2, 3)
+    )
 })
